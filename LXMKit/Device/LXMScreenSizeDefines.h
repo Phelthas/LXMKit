ac067f2cfd4c31c8808a756c7628314ba12a1e44
billthaslu <billthaslu@tencent.com>
2025-05-06 21:44:32 +0800

    更新屏幕尺寸

--- LXMKit/Device/LXMScreenSizeDefines.h
+++ LXMKit/Device/LXMScreenSizeDefines.h
@@ -29,42 +29,48 @@
 #define kScreenSize393x852     CGSizeMake(393, 852)
 #define kScreenSize428x926     CGSizeMake(428, 926)
 #define kScreenSize430x932     CGSizeMake(430, 932)
+#define kScreenSize402x874     CGSizeMake(402, 874)
+#define kScreenSize440x956     CGSizeMake(440, 956)
 
 #define kIPadScreenSize768x1024      CGSizeMake(768, 1024) // 3 : 4
 #define kIPadScreenSize834x1112      CGSizeMake(834, 1112) // 3 : 4
 #define kIPadScreenSize1024x1366     CGSizeMake(1024, 1366) // 3 : 4
 #define kIPadScreenSize834x1194      CGSizeMake(834, 1194)  // 0.698
 #define kIPadScreenSize810x1080      CGSizeMake(810, 1080)  // 3 : 4
+#define kIPadScreenSize744x1133      CGSizeMake(744, 1133)  // 0.656
+#define kIPadScreenSize820x1180      CGSizeMake(820, 1180)  // 0.695
+#define kIPadScreenSize834x1210      CGSizeMake(834, 1210)  // 0.689
+#define kIPadScreenSize1032x1376     CGSizeMake(1032, 1376)  // 3 : 4
 
 #pragma mark ----------------------------------现有机型尺寸----------------------------------
 // https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
 // https://www.ios-resolution.com/
 
-#define kScreenSizeiPhone4     kScreenSize320x480      //2x
-#define kScreenSizeiPhone4S    kScreenSize320x480      //2x
+#define kScreenSizeiPhone4                   kScreenSize320x480      //2x
+#define kScreenSizeiPhone4S                  kScreenSize320x480      //2x
 
-#define kScreenSizeiPhone5     kScreenSize320x568      //2x
-#define kScreenSizeiPhone5C    kScreenSize320x568      //2x
-#define kScreenSizeiPhone5S    kScreenSize320x568      //2x
-#define kScreenSizeiPhoneSE    kScreenSize320x568      //2x
+#define kScreenSizeiPhone5                   kScreenSize320x568      //2x
+#define kScreenSizeiPhone5C                  kScreenSize320x568      //2x
+#define kScreenSizeiPhone5S                  kScreenSize320x568      //2x
+#define kScreenSizeiPhoneSE                  kScreenSize320x568      //2x
 
-#define kScreenSizeiPhone6         kScreenSize375x667      //2x
-#define kScreenSizeiPhone6S        kScreenSize375x667      //2x
-#define kScreenSizeiPhone7         kScreenSize375x667      //2x
-#define kScreenSizeiPhone8         kScreenSize375x667      //2x
-#define kScreenSizeiPhoneSE2       kScreenSize375x667      //2x
-#define kScreenSizeiPhoneSE3       kScreenSize375x667      //2x
+#define kScreenSizeiPhone6                   kScreenSize375x667      //2x
+#define kScreenSizeiPhone6S                  kScreenSize375x667      //2x
+#define kScreenSizeiPhone7                   kScreenSize375x667      //2x
+#define kScreenSizeiPhone8                   kScreenSize375x667      //2x
+#define kScreenSizeiPhoneSE2                 kScreenSize375x667      //2x
+#define kScreenSizeiPhoneSE3                 kScreenSize375x667      //2x
 
-#define kScreenSizeiPhone6Plus     kScreenSize414x736      //3x
-#define kScreenSizeiPhone6SPlus    kScreenSize414x736      //3x
-#define kScreenSizeiPhone7Plus     kScreenSize414x736      //3x
-#define kScreenSizeiPhone8Plus     kScreenSize414x736      //3x
+#define kScreenSizeiPhone6Plus               kScreenSize414x736      //3x
+#define kScreenSizeiPhone6SPlus              kScreenSize414x736      //3x
+#define kScreenSizeiPhone7Plus               kScreenSize414x736      //3x
+#define kScreenSizeiPhone8Plus               kScreenSize414x736      //3x
 
-#define kScreenSizeiPhoneX                kScreenSize375x812      //3x
-#define kScreenSizeiPhoneXS               kScreenSize375x812      //3x
-#define kScreenSizeiPhone11Pro            kScreenSize375x812      //3x
-#define kScreenSizeiPhone12Mini           kScreenSize375x812      //3x
-#define kScreenSizeiPhone13Mini           kScreenSize375x812      //3x
+#define kScreenSizeiPhoneX                   kScreenSize375x812      //3x
+#define kScreenSizeiPhoneXS                  kScreenSize375x812      //3x
+#define kScreenSizeiPhone11Pro               kScreenSize375x812      //3x
+#define kScreenSizeiPhone12Mini              kScreenSize375x812      //3x
+#define kScreenSizeiPhone13Mini              kScreenSize375x812      //3x
 
 #define kScreenSizeiPhoneXR                  kScreenSize414x896      //2x
 #define kScreenSizeiPhoneXSMax               kScreenSize414x896      //3x
@@ -80,6 +86,7 @@
 #define kScreenSizeiPhone14Pro               kScreenSize393x852      //3x
 #define kScreenSizeiPhone15                  kScreenSize393x852      //3x
 #define kScreenSizeiPhone15Pro               kScreenSize393x852      //3x
+#define kScreenSizeiPhone16                  kScreenSize393x852      //3x
 
 #define kScreenSizeiPhone12ProMax            kScreenSize428x926      //3x
 #define kScreenSizeiPhone13ProMax            kScreenSize428x926      //3x
@@ -88,5 +95,9 @@
 #define kScreenSizeiPhone14ProMax            kScreenSize430x932      //3x
 #define kScreenSizeiPhone15PLus              kScreenSize430x932      //3x
 #define kScreenSizeiPhone15ProMax            kScreenSize430x932      //3x
+#define kScreenSizeiPhone16PLus              kScreenSize430x932      //3x
+
+#define kScreenSizeiPhone16Pro               kScreenSize402x874      //3x
+#define kScreenSizeiPhone16ProMax            kScreenSize440x956      //3x
 
 #endif /* LXMScreenSizeDefines_h */
